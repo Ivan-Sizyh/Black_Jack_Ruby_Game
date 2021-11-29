bf5e9a9cbf324ffd5bc54f7286982368b44fa625
Ivan Sizyh <ivansizyh26@gmail.com>
2021-11-29 23:45:59 +0700

    Add Player class

--- Card.rb
+++ Card.rb
@@ -2,12 +2,12 @@ class Card
   attr_reader :cards
 
   def initialize
-    @cards = []
+    @desk_cards = []
     create_cards
   end
 
   def give_card
-
+    @cards.delete(@desk_cards.sample)
   end
 
   def create_cards
@@ -15,10 +15,10 @@ class Card
     card_suits = %W[\u2660 \u2665 \u2666 \u2663] #1 - Пики, 2 - Черви, 3 - Буби, 4 - Крести
 
     card_denominations.each do |card_denomination|
-      @cards << "#{card_denomination}#{card_suits[0]}"
-      @cards << "#{card_denomination}#{card_suits[1]}"
-      @cards << "#{card_denomination}#{card_suits[2]}"
-      @cards << "#{card_denomination}#{card_suits[3]}"
+      @desk_cards << "#{card_denomination}#{card_suits[0]}"
+      @desk_cards << "#{card_denomination}#{card_suits[1]}"
+      @desk_cards << "#{card_denomination}#{card_suits[2]}"
+      @desk_cards << "#{card_denomination}#{card_suits[3]}"
     end
   end
 end
--- Player.rb
+++ Player.rb
@@ -1,1 +1,29 @@
+class Player
+
+  @@game_bank = 0
+
+  attr_accessor :cards, :score
+  attr_reader :bank, :name
+
+  def initialize
+    @score = 0
+    @cards = []
+    @bank = 100
+    @name
+  end
+
+  def get_card(desk)
+    @cards << desk.give_card
+  end
+
+  def make_bet
+    if @bank > 0
+      @bank -= 10
+      @@game_bank += 10
+    else
+      puts "На балансе не хватает денег для ставки!"
+    end
+  end
+
+end
 
